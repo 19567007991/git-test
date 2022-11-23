5782d9f92724980be0fe16530221586aeb58d492
hulufei <3317339594@qq.com>
2022-11-23 11:39:02 +0800

    master second commit 3

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hot-fix update");
         System.out.println("master update 2");
         System.out.println("hot-fix update2");
+        System.out.println("1234567");
     }
 }
